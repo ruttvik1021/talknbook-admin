62326cddc6cecfe6ea36deac9be5ed1f1da8765a
Ruttvik21 <rkhollam21@live.com>
2023-12-06 21:06:36 +0530

    updated page route middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,7 +3,7 @@ import type { NextRequest } from "next/server";
 import * as jose from "jose";
 import { ROLES } from "./utils/constants";
 
-const protectedRoutes = ["/dashboard"];
+const protectedRoutes = ["/dashboard", "/users", "/master-data/**"];
 
 export default async function middlerware(req: NextRequest) {
   const token = req.cookies.get("token")?.value || "";
@@ -13,9 +13,13 @@ export default async function middlerware(req: NextRequest) {
   try {
     const decoded = await jose.jwtVerify(token, signature);
     const isSuperAdmin = decoded.payload?.role === ROLES.SUPERADMIN;
+    const dashboardUrl = new URL("/dashboard", req.nextUrl.origin);
     if (!isSuperAdmin && protectedRoutes.includes(req.nextUrl.pathname)) {
       const absoluteUrl = new URL("/login", req.nextUrl.origin);
       return NextResponse.redirect(absoluteUrl.toString());
+    }
+    if (isSuperAdmin && req.nextUrl.pathname === "/") {
+      return NextResponse.redirect(dashboardUrl.toString());
     }
     return;
   } catch (error) {
