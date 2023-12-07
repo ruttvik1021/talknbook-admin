ab7b11b49e4bdc82c01dd58a867b1044a2fb0c9f
Ruttvik21 <rkhollam21@live.com>
2023-12-08 01:45:23 +0530

    updated urls

--- src/utils/urls.ts
+++ src/utils/urls.ts
@@ -1,12 +1,6 @@
-export const unAuthBaseUrl =
-  process.env.ENVTYPE === "LOCAL"
-    ? "http://localhost:3000"
-    : process.env.DEV_BACKEND_URL;
+export const unAuthBaseUrl = "https://talk-n-book.onrender.com";
 
-export const authBaseUrl =
-  process.env.ENVTYPE === "LOCAL"
-    ? "http://localhost:3000/api"
-    : `${process.env.DEV_BACKEND_URL}/api`;
+export const authBaseUrl = "https://talk-n-book.onrender.com/api";
 
 export const urls = {
   sentOtp: "/otp/admin/sendotp",
